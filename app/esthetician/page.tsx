4ac0557e6bd8868123d8ed8735c69b62d4e931c1
Ladwa14 <kartikladwa@gmail.com>
2026-06-01 22:39:14 +0530

    image first added in Esthetician

--- app/esthetician/page.tsx
+++ app/esthetician/page.tsx
@@ -6,10 +6,13 @@ import Footer from "../sections/Footer";
 import Header from "../sections/Header";
 
 const images = [
-  "/Blanc_website_2.webp",
-  "/Smile.webp",
-  "/Above_the_fold_3.webp",
-  "/Above_the_fold_4.webp",
+
+  "/The_Esthetician_1st_image.webp",
+
+  // "/Blanc_website_2.webp",
+  // "/Smile.webp",
+  // "/Above_the_fold_3.webp",
+  // "/Above_the_fold_4.webp",
 ];
 
 export default function Page() {
