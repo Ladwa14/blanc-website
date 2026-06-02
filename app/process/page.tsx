85d8867963d484e7d161431cb1907cd61d79ad6c
Ladwa14 <kartikladwa@gmail.com>
2026-06-02 22:40:48 +0530

    our process images updated

--- app/process/page.tsx
+++ app/process/page.tsx
@@ -6,10 +6,12 @@ import Footer from "../sections/Footer";
 import Header from "../sections/Header";
 
 const images = [
-  "/Blanc_website_2.webp",
-  "/Smile.webp",
-  "/Above_the_fold_3.webp",
-  "/Above_the_fold_4.webp",
+  "/Our_process_1.webp",
+  "/Our_process_2.webp"
+  // "/Blanc_website_2.webp",
+  // "/Smile.webp",
+  // "/Above_the_fold_3.webp",
+  // "/Above_the_fold_4.webp",
 ];
 
 export default function Page() {
