bc8b1d1a3f5d7b0b35e06a53d88772760f41e54a
Ladwa14 <kartikladwa@gmail.com>
2026-09-21 21:52:46 +0530

    blog for digital smile image updated

--- app/blog/digital-smile-design-procedure/page.tsx
+++ app/blog/digital-smile-design-procedure/page.tsx
@@ -28,7 +28,7 @@ export default function DigitalSmileDesignPage() {
           {/* BLOG IMAGE */}
           <div className="max-w-[760px] mx-auto">
             <img
-              src="/How_Do_Veneers_for_Gap_Teeth_Work.webp"
+              src="/digital-smile-design-procedure.webp"
               alt="Digital Smile Design Procedure"
               className="w-full aspect-[16/9] object-cover"
             />
